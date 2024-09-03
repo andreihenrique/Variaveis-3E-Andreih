99f6581c94bbb628666e7d0305145b4b262d22e3
andreihenrique <128196140+andreihenrique@users.noreply.github.com>
2024-09-03 22:57:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const nomeCompleto = "Andrei Henrique da Silva ";
+const anoNascimento = 2006;
+let anoAtual = 2024;
+let idade = 18;
+
+console.log(" Olá meu nome é " + nomeCompleto + " esse ano pretendo termina o ensino medio " + anoAtual + " vou fazer  " + idade + " e vou comemorar o natal em outra cidade" + anoNascimento);
+
+anoAtual = anoAtual + 1;
+
+
+console.log(`oi eu sou ${nomeCompleto} no proximo ano pretendo servir o exercito ${anoAtual} , vou fazer ${idade}  no proximo ano e vou comemorar o natal na minha cidade natal, aonde eu nasci em ${anoNascimento} ` )
+
+const listaDeViagens = new Array (
+  `chile`,
+  `italia`,
+  `korea do sul`,
+  `Japão`,
+  `Uruguay`,
+);
+
+console.log(listaDeViagens);
